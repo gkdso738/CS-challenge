22368ca1b59fa0d7b49a65710baa75c3278c6fc9
gkdso738 <54734860+gkdso738@users.noreply.github.com>
2020-09-26 01:57:07 +0800

    Update sort.py

--- codeitsuisse/routes/sort.py
+++ codeitsuisse/routes/sort.py
@@ -10,8 +10,8 @@ logger = logging.getLogger(__name__)
 @app.route('/sort', methods=['POST'])
 def evaluate():
     data = request.get_json();
-    logging.info("data sent for evaluation {}".format(data))
+    #logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("input");
     inputValue.sort()
-    logging.info("My result :{}".format(inputValue))
+    #logging.info("My result :{}".format(inputValue))
     return json.dumps(inputValue);
