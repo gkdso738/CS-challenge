3d92f0708fe27e38cd81a02ebec7cfe1d2b083fa
gkdso738 <54734860+gkdso738@users.noreply.github.com>
2020-09-26 17:35:23 +0800

    Update intelligent_farming.py

--- codeitsuisse/routes/intelligent_farming.py
+++ codeitsuisse/routes/intelligent_farming.py
@@ -8,32 +8,26 @@ from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
-def solve(s):
-    A = 0
-    T = 0
-    C = 0
-    G = 0
-    for i in range(len(s)):
-        if s[i] == 'A': A += 1
-        if s[i] == 'T': T += 1
-        if s[i] == 'C': C += 1
-        if s[i] == 'G': G += 1
+def compute(A,T,C,G,rnd):
     ans = ""
+    for i in range(rnd):
+        if A > 2:
+            ans = ans + "AA"
+            A -= 2
+        ans = ans + "TACG"
+        score += 15
+        A -= 1
+        T -= 1
+        C -= 1
+        G -= 1
     while C>1:
         if A > 2:
             ans = ans + "AA"
             A -= 2
         ans = ans + "CC"
         C -= 2
-    if C == 1 and A > 0 and T > 0 and G > 0:
-        if A > 2:
-            ans = ans + "AA"
-            A -= 2
-        ans = ans + "TACG"
-        A -= 1
-        T -= 1
-        C -= 1
-        G -= 1
+        scopre += 25
+
     while A > 2 and T > 0:
         ans = ans + "AA"
         A -= 2
@@ -50,10 +44,31 @@ def solve(s):
     while G > 0:
         ans = ans + "G"
         G -= 1
+    score -= A // 3 * 10
     while A > 0:
         ans = ans + "A"
         A -= 1
-    return ans
+    return score, ans
+
+def solve(s):
+    score = 0
+    A = 0
+    T = 0
+    C = 0
+    G = 0
+    for i in range(len(s)):
+        if s[i] == 'A': A += 1
+        if s[i] == 'T': T += 1
+        if s[i] == 'C': C += 1
+        if s[i] == 'G': G += 1
+    mx = -1000000000
+    cand = ""
+    for i in range(min(min(A,T),min(C,G))):
+        score, ans = compute(A,T,C,G,i)
+        if score > mx:
+            mx = score
+            cand = ans
+    return cand
     
 
 @app.route('/intelligent-farming', methods=['POST'])
