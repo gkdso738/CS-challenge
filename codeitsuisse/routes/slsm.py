04e80ec357a0218e4d5e9a3958b854d43439b20a
gkdso738 <54734860+gkdso738@users.noreply.github.com>
2020-09-26 20:31:10 +0800

    Update slsm.py

--- codeitsuisse/routes/slsm.py
+++ codeitsuisse/routes/slsm.py
@@ -9,7 +9,7 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 
 
-def solve(n, mp, rnd):
+def solve(n, mp, extra_roll, rnd):
     ans = []
     cur = 1
     while cur+6 < n:
@@ -23,15 +23,21 @@ def solve(n, mp, rnd):
         cur = mx
         for _ in range(rnd):
             ans.append(cand)
+            if extra_roll[cur] != 0:
+                ans.append(extra_roll[cur])
     for i in range(1,7):
         if mp[cur+i] != n:
             cur = mp[cur+i]
             for _ in range(rnd-1):
                 ans.append(i)
+                if extra_roll[cur] != 0:
+                    ans.append(extra_roll[cur])
             break
     for i in range(1,7):
         if mp[cur+i] == n:
             ans.append(i)
+            if extra_roll[cur+i] != 0:
+                ans.append(extra_roll[cur+i])
             break
     while cur < n:
         mx = -1
@@ -43,41 +49,49 @@ def solve(n, mp, rnd):
         cur = mx
         for _ in range(rnd-1):
             ans.append(cand)
+            if extra_roll[cur] != 0:
+                ans.append(extra_roll[cur])
     return ans
 def build(n, jumps):
     kw = []
     mp = {}
+    extra_roll = {}
+    for i in range(n+6):
+        extra_roll[i] = 0
     for element in jumps:
         x,y = element.split(':')
         x = int(x)
         y = int(y)
         if x == 0:
             kw.append(y)
-            mp[y] = mp[y-1]
+            mp[y] = y+6
+            extra_roll[y] = 6
         else:
             kw.append(x)
             if y == 0:
-                mp[x] = x+6
+                mp[x] = mp[x-1]
+                extra_roll[x] = 1
             else:
                 mp[x] = y
     for i in range(n+1):
         if i not in kw:
             mp[i] = i
+
     for i in range(n+1,n+6):
         mp[i] = mp[n+n-i]
-    return mp
+        extra_roll[i] = extra_roll[n+n-i]
+    return mp, extra_roll
 
 @app.route('/slsm', methods=['POST'])
 def evaluate_slsm():
-    
+
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    
+
     n = data["boardSize"]
     rnd = data["players"]
     mp = build(n,data["jumps"])
-    result = solve(n,mp,rnd)
-    
+    result = solve(n,mp,extra_roll,rnd)
+
     # logging.info("My result :{}".format(result))
     return jsonify(result);
-
